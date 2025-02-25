d20c598ca648a8ccfc559175b77f46dd47e1eb90
Ken Ovienadu <kenovienadu@gmail.com>
2025-02-26 00:09:54 +0100

    refactor: simplify page rendering functions in handlers

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,15 +1,7 @@
 import { Request, Response } from 'express'
 
-const renderHomePage = (_: Request, res: Response) => {
-    res.render("home");
-}
-
-const renderServicesPage = (_: Request, res: Response) => {
-    res.render("services");
-}
-
-const renderContactPage = (_: Request, res: Response) => {
-    res.render("contact");
-}
+const renderHomePage = (_: Request, res: Response) => res.render("home");
+const renderServicesPage = (_: Request, res: Response) =>  res.render("services");
+const renderContactPage = (_: Request, res: Response) => res.render("contact");
 
 export default { renderHomePage, renderContactPage, renderServicesPage };
